c4ab874fd6c898eb1fb9ab9b7eb3271e00889434
Gil Rodrigues <gil.rodrigues286@gmail.com>
2020-07-04 11:57:12 +0100

    usar serviço

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
@@ -11,23 +12,21 @@ appointmentsRouter.get('/', (req, res) => {
 });
 
 appointmentsRouter.post('/', (req, res) => {
-  const { provider, date } = req.body;
+  try {
+    const { provider, date } = req.body;
 
-  const parsedDate = startOfHour(parseISO(date));
+    const parsedDate = parseISO(date);
 
-  const findAppointmentInSameDate = appointmentsRepository.findByDate(
-    parsedDate
-  );
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository
+    );
 
-  if (findAppointmentInSameDate) {
-    return res
-      .status(400)
-      .json({ message: 'This appointment is already booked' });
+    const appointment = createAppointment.run({ provider, date: parsedDate });
+
+    return res.json(appointment);
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
   }
-
-  const appointment = appointmentsRepository.create(provider, parsedDate);
-
-  return res.json(appointment);
 });
 
 export default appointmentsRouter;
